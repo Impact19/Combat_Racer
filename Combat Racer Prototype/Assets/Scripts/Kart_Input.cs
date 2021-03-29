a896b40faffbc35eb1a7cc3a87b3863502f68775
Impact19 <Isaiahrivers444@gmail.com>
2021-03-29 00:11:23 -0400

    Create Kart_Input.cs

---
+++ Combat Racer Prototype/Assets/Scripts/Kart_Input.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Kart_Input : MonoBehaviour
+{
+   
+// Testing GitHub Commit!
+  [SerializeField]private float kartSpeed, kartMaxSpeed; 
+  [SerializeField]protected float accleration;
+  [SerializeField]private float turnSpeed; 
+  [SerializeField]private Rigidbody kartRb;
+  [SerializeField]private bool canMove; 
+  [SerializeField]
+    private void Update()
+    {
+        if (Input.GetButton("Accelerate"))  
+        {
+            Kart_Accelrate();
+        }
+        Kart_Steer(); 
+    }
+
+    private void Kart_Accelrate()  
+    {
+        
+        Debug.Log("Kart Acclerating");
+            if (kartSpeed <= kartMaxSpeed)
+            {
+               kartSpeed = accleration * Time.deltaTime;
+            }
+        
+        transform.Translate(0f, 0f, kartSpeed);
+    }
+
+    private void Kart_Steer()
+    { 
+
+        transform.Translate(Input.GetAxis("Horizontal") * turnSpeed, 0f, 0f);
+    }
+
+}
+
